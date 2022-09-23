e3510e80bc2acf1b8d84ed4ce2c7b807464ccd0b
andriifromlviv <95188832+andriifromlviv@users.noreply.github.com>
2022-09-23 14:38:12 +0300

    start work with drop down list

--- cypress/e2e/IndusTrack.cy.js
+++ cypress/e2e/IndusTrack.cy.js
@@ -30,5 +30,15 @@ describe('Invoice price check', () => {
     cy.xpath(sel.agapeMechanical).click()
     cy.xpath(sel.proceedButton).click()
 
+    //Check, that total sum is zero before adding products
+    cy.get("td[style='color: black;']").should('contain', 0)
+
+    cy.get("[placeholder='Type to search']").click()
+    cy.get("mat-option[class='mat-option mat-focus-indicator ng-star-inserted']").eq(2).click()
+
+    cy.get("[placeholder='Type to search']").click()
+    let a = cy.get("mat-option[class='mat-option mat-focus-indicator ng-star-inserted']").its('length')
+    console.log(a)
+
   })
 })
